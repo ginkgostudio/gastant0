d908988f6df19937bfecd079976282d2d7416487
leonardo pinzon <leopinzon@gmail.com>
2019-12-02 11:45:14 -0300

    Expense form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Expense from './components/Expense';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Expense />
     </div>
   );
 }
---
+++ src/components/Expense.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Expense extends Component{
+    render(){
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Expense;
