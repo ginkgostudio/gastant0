5d41a59ea51bf75eddc0d4800167a42749a37dbe
Natalia Varela <natalia.varela01@gmail.com>
2019-12-03 13:12:02 -0300

    Currency selector in expense component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 
 class Expense extends Component{
+    componentDidMount() {
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('select');
+            var instances = M.FormSelect.init(elems, options);
+        });
+    }
     render(){
         return (
             <div>
-                
-            </div>
-        );
-    }
-}
 
-export default Expense;
+                <div className="row">
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="Importe" type="number" className="validate" />
+                                <label for="Importe">Importe</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="concepto" type="text" className="validate" />
+                                <label for="concepto">Concepto</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <select>
+                                <option value="" disabled selected>Choose your option</option>
+                                <option value="1">ARS></option>
+                                <option value="2">COP</option>
+                                <option value="3">USD</option>
+                                </select>
+                                <label>Materialize Select</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+            );
+        }
+    }
+    
+    export default Expense;
