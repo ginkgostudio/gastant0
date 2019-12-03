ec3e7eedeac91088efb39d43e94f705b1eb321bf
Natalia Varela <natalia.varela01@gmail.com>
2019-12-03 16:17:16 -0300

    Payment method field in expense component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -6,13 +6,15 @@ class Expense extends Component{
     componentDidMount() {
         document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('select');
-            let options = {
-                inDuration: 300,
-                outDuration: 300,
-                hover: true, // Activate on hover
-                coverTrigger: false, // Displays dropdown below the button
-            };
-            var instances = M.FormSelect.init(elems, options);
+            var instances = M.FormSelect.init(elems);
+            var options = {
+                autoClose: true,
+                defaultDate: new Date(),
+                setDefaultDate: true
+
+            }
+            var elems1 = document.querySelectorAll('.datepicker');
+            var instances = M.Datepicker.init(elems1, options);
         });
     }
     render(){
@@ -34,14 +36,37 @@ class Expense extends Component{
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s12">
+                            <div className="input-field col s6">
                                 <select>
-                                <option value="" disabled selected>Choose your option</option>
-                                <option value="1">ARS></option>
+                                <option value="1" selected>ARS</option>
                                 <option value="2">COP</option>
                                 <option value="3">USD</option>
                                 </select>
-                                <label>Materialize Select</label>
+                                <label>Moneda</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <select>
+                                <option value="1" selected>Efectivo/débito</option>
+                                <option value="2">Tarjeta BBVA</option>
+                                <option value="2">Tarjeta Santander</option>
+                                <option value="2">Tarjeta Payoneer</option>
+                                </select>
+                                <label>Medio de pago</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="expense-date" type="text" class="datepicker"/>
+                                <label for="expense-date">Fecha del gasto</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s6">
+                                <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                    <i className="material-icons right">send</i>
+                                </button>
                             </div>
                         </div>
                     </form>
