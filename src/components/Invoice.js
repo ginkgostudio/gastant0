7bf1ae689ed1b15d5c42ad1e2da97ab4b48d3f8d
Natalia Varela <natalia.varela01@gmail.com>
2020-03-31 18:25:12 -0300

    Use State hook for invoice

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import 'date-fns';
-import MaterialUIPicker from './common/Datepicker'
+import Datepicker from './common/Datepicker'
 import Currency from './common/Currency'
 import InvoiceNumber from './common/InvoiceNumber'
 import Description from './common/Description'
@@ -10,39 +10,49 @@ import Amount from './common/Amount'
 
 export default function Invoice() {  
 
+    const [invoiceNumber, setInvoiceNumber] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [amount, setAmount] = React.useState('');
+    const [currency, setCurrency] = React.useState('');
+    const [selectedDate, setSelectedDate] = React.useState(Date.now());
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
         return (
             
             <div className="row">
                 <h3>Invoice </h3>
-                <form action="" className="col s12">
+                <form onSubmit={handleSubmit} className="col s12">
                     <div className="row">
                         <div className="input-field col s6">
-                            <InvoiceNumber />
+                            <InvoiceNumber handleChange={setInvoiceNumber}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
-                            <Description />
+                            <Description handleChange={setDescription} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
-                            <Amount />
+                            <Amount handleChange={setAmount}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
-                            <Currency /> 
+                            <Currency handleChange={setCurrency} currency={currency} /> 
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
-                            <MaterialUIPicker />
+                            <Datepicker handleChange={setSelectedDate} selectedDate={selectedDate}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col s6">
-                            <Button variant="contained" color="secondary">Submit</Button>
+                            <Button variant="contained" type="submit" color="secondary">Submit</Button>
                         </div>
                     </div>
                 </form>
